387770c25326c02fd77c2f125f37cc3846600533
SaicharanE <saicharan.epparapally@gmail.com>
2022-07-26 13:05:58 +0530

    Practise sessions

---
+++ src/com/genera/Nothing.java
@@ -0,0 +1,16 @@
+package com.genera;
+
+
+
+public class Nothing {
+
+    public void phone1(){
+        String S = "Nothing Phone (1)";
+        System.out.println(S);
+    }
+
+    public static void main(String[] args) {
+    Nothing n = new Nothing();
+    n.phone1();
+    }
+}
